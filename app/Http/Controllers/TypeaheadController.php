a74b9cc037c91b45f4af9858032f0aeccf133028
Mark O <mark.oconnor14@gmail.com>
2021-10-22 02:53:44 +0700

    test schema create table with typehead controller

--- app/Http/Controllers/TypeaheadController.php
+++ app/Http/Controllers/TypeaheadController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 use App\Models\Link;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class TypeaheadController extends Controller
 {
     public function index()
     {
+        
+        // if (Schema::hasTable('datas')) {
+        //     echo "You have a datas table";
+        // } else {
+
+        //     echo "There is no datas table";
+        //     Schema::create('datas', function (Blueprint $table) {
+        //                 $table->increments('id');
+        //                 $table->string('name');
+        //                 $table->string('airline');
+        //                 $table->timestamps();
+        //             });
+        //     echo "But we made a table for you :)";
+        // }
         return view('welcome');
     }
 
