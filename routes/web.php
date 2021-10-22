077ca98d7395a88eb541400c49585454f3f317b4
Mark O <mark.oconnor14@gmail.com>
2021-10-22 12:52:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,23 @@ use App\Http\Controllers\TypeaheadController;
 
 Route::get('/', [App\Http\Controllers\TypeaheadController::class, 'index']);
 
-Route::get('/autocomplete', [App\Http\Controllers\TypeaheadController::class, 'autocomplete'])->name('autocomplete');
+// comment out automplete for now. try to fix it later
+// Route::get('/autocomplete', [App\Http\Controllers\TypeaheadController::class, 'autocomplete'])->name('autocomplete');
 
-// I think I could change this to '/' to make it the default home page
-// but it's not necessary
-// and besides, I can put the URL input in welcome
+// route to scraped results page
+// displays the confirmed cases, recovered cases, and deaths stats
+// for worldwide or specific countries
+// displays one results page at a time
 Route::get('scraper', [\App\Http\Controllers\ScraperController::class, 'scraper'])->name('scraper') ;
 
 
 // route for paginated links page
-// this route will also send the link data to the database
+// the pagination is done with use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+// the results are scraped
+// and then paginated on the screen
 Route::get('list', [App\Http\Controllers\ListScraperController::class, 'list'])->name('list') ;
+
+
+
 
 
